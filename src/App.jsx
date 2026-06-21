d467233bee2fe33f5e6097bad654fa8d7cefe526
Ajay Panwar <panwarajay149@gmail.com>
2026-06-21 20:54:04 +0530

    mobile version had a scroll issue (fixed)

--- src/App.jsx
+++ src/App.jsx
@@ -210,6 +210,10 @@ function App() {
   }, [])
 
   useEffect(() => {
+    ScrollTrigger.refresh()
+  }, [garageExpanded])
+
+  useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
